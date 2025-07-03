0cf676a98156c6bc6103df023d354e3a5c32e99b
John Peters <jppeters3@wisc.edu>
2025-07-03 14:28:32 -0500

    scoring script

---
+++ log_score.py
@@ -0,0 +1,47 @@
+import wandb
+import os
+import json
+import subprocess
+import polars as pl
+import argparse
+wandb.Settings(quiet=True)
+
+score_directory = "/mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_scores/"
+
+def main(run_id, iteration, checkpoint_score_path):                
+    score_csv = pl.read_csv(checkpoint_score_path) 
+    allen_score = score_csv.filter(pl.col('dataset')=="Allen", pl.col("task")=="Task_two")['f1_score_macro'].sum()/3 # .item is eqv here
+    hpa_score = score_csv.filter(pl.col('dataset')=="HPA", pl.col("task").is_in(['Task_two', "Task_three"]))['f1_score_macro'].sum()/6
+    cp_score = score_csv.filter(pl.col('dataset')=="CP", pl.col("task").is_in(['Task_two', "Task_three", "Task_three"]))['f1_score_macro'].sum()/9
+    
+    chammi_score = allen_score + hpa_score + cp_score
+    
+    try:
+        wandb.init(
+            project="dinov2_chammi",
+            id=run_id,
+            resume="must",
+            reinit='default'
+        )
+        wandb.define_metric("eval/step")
+        wandb.define_metric("eval/*", step_metric="eval/step")    
+        wandb.log(
+            {
+                "eval/CHAMMI Score": chammi_score, 
+                "eval/step": iteration
+            }
+        )
+
+        wandb.finish()
+    except:
+        print("Score was not logged to wandb.")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some arguments.")
+    parser.add_argument("--run-id", type=str, required=True, help="ID of the run")
+    parser.add_argument("--iteration", type=int, required=True, help="Iteration number to evaluate at")
+    parser.add_argument("--score-path", type=str, required=True, help="Path to the CHAMMI score file")
+
+    args = parser.parse_args()
+    
+    main(args.run_id, args.iteration, args.score_path)
---
+++ mass_scoring.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import sys
+from multiprocessing import Queue, Pool, Manager
+from functools import partial
+
+SCORES_ROOT = '/mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_scores'
+FEATURES_ROOT    = '/mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_features'
+
+def main():
+    scored_models = os.listdir(SCORES_ROOT)
+    evaled_models = os.listdir(FEATURES_ROOT)
+    
+    checkpoints_to_eval = []
+    for model in evaled_models:    
+        if model not in scored_models:
+            checkpoints:list[str] = os.listdir(os.path.join(FEATURES_ROOT, model))
+            checkpoints = [tuple(checkpoint.rsplit('_', 1)) for checkpoint in checkpoints]
+            checkpoints.sort(key=lambda x: int(x[-1]))
+            for name, check in checkpoints:
+                script = f"python morphem/benchmark.py --root-dir /scr/data/CHAMMI/dataset --dest-dir /mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_scores/{model}/{name}_{check} --feature-dir /mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_features/{model}/{name}_{check} --feature-file pretrained_dinov2_vit_features.npy"                
+                result = subprocess.run(
+                    script,
+                    shell=True,
+                    capture_output=False,
+                    check=True,
+                    text=True
+                )
+                
+                log_script = f"python ./log_score.py --run-id {model} --iteration {check} --score-path /mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_scores/{model}/{name}_{check}/knn_l2_full_results.csv"                
+                result = subprocess.run(
+                    log_script,
+                    shell=True,
+                    capture_output=False,
+                    check=True,
+                    text=True
+                )
+
+    print(checkpoints_to_eval)
+    
+if __name__ == "__main__":
+    main()
