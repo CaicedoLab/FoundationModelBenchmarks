195bcb60684cc69dee32eb15fedb1b7b54e73ff9
John Peters <jgpeters3@wisc.edu>
2025-07-15 12:21:11 -0500

    Updated gitignore to ignore out and err chtc logs. Updated mass scoring to support dry runs

--- mass_scoring.py
+++ mass_scoring.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 from multiprocessing import Queue, Pool, Manager
 from functools import partial
+import argparse
 
 # SCORES_ROOT = '/mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_scores'
 # FEATURES_ROOT    = '/mnt/cephfs/mir/jcaicedo/projects/foundation_models_and_benchmarking/chammi_features'
@@ -12,7 +13,7 @@ SCORES_ROOT = '/hdd/jcaicedo/projects/foundation_models_and_benchmarking/chammi_
 FEATURES_ROOT    = '/hdd/jcaicedo/projects/foundation_models_and_benchmarking/chammi_features'
 ROOT_DIR = '/hdd/jcaicedo/projects/foundation_models_and_benchmarking/chammi_metadata'
 
-def main():
+def main(dry_run: bool):
     scored_models = os.listdir(SCORES_ROOT)
     evaled_models = os.listdir(FEATURES_ROOT)
     
@@ -28,26 +29,33 @@ def main():
             dest_dir = os.path.join(SCORES_ROOT, model, f'{name}_{check}')
             feat_dir = os.path.join(FEATURES_ROOT, model, f'{name}_{check}')
             
-            script = f"python morphem/benchmark.py --root-dir {ROOT_DIR} --dest-dir {dest_dir} --feature-dir {feat_dir} --feature-file pretrained_dinov2_vit_features.npy"                
-            result = subprocess.run(
-                script,
-                shell=True,
-                capture_output=False,
-                check=True,
-                text=True
-            )
+            if dry_run:
+                print(model, f'{name}_{check}')
+            else:
+                script = f"python morphem/benchmark.py --root-dir {ROOT_DIR} --dest-dir {dest_dir} --feature-dir {feat_dir} --feature-file pretrained_dinov2_vit_features.npy"                
+                result = subprocess.run(
+                    script,
+                    shell=True,
+                    capture_output=False,
+                    check=True,
+                    text=True
+                )
             
-            score_path = os.path.join(dest_dir, 'knn_l2_full_results.csv')
-            log_script = f"python ./log_score.py --run-id {model} --iteration {check} --score-path {score_path}"                
-            result = subprocess.run(
-                log_script,
-                shell=True,
-                capture_output=False,
-                check=True,
-                text=True
-            )
+                score_path = os.path.join(dest_dir, 'knn_l2_full_results.csv')
+                log_script = f"python ./log_score.py --run-id {model} --iteration {check} --score-path {score_path}"                
+                result = subprocess.run(
+                    log_script,
+                    shell=True,
+                    capture_output=False,
+                    check=True,
+                    text=True
+                )
 
     print(checkpoints_to_eval)
     
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="My program")
+    parser.add_argument("-d", "--dry-run", action="store_true", help="Perform a dry run without making actual changes")
+    args = parser.parse_args()
+    
+    main(args.dry_run)
