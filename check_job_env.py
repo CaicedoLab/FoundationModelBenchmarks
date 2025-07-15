292d47221ff433eda6e4ee18b3c51233f73e4f3c
John Peters <jgpeters3@wisc.edu>
2025-07-15 10:25:41 -0500

    more robust check job script

--- check_job_env.py
+++ check_job_env.py
@@ -2,23 +2,26 @@ import subprocess
 
 def main():
     result = subprocess.run(
-                    f"condor_history jgpeters3 -limit 300",
+                    f"condor_history jgpeters3 -limit 100",
                     shell=True,
                     capture_output=True,
                     check=True,
                     text=True
                 )
-    results = result.stdout.split('\n')
+    results = result.stdout.split('\n')[1:-1]
     for result in results[1:]:
-        job_id = result.split()[0]
-        result = subprocess.run(
-                    f"condor_history -l {job_id} | grep MachineAttrMachine0",
-                    shell=True,
-                    capture_output=True,
-                    check=True,
-                    text=True
-                )
-        print(result.stdout.strip(), job_id)
+        time_values = map(int, result.split()[4][2:].split(":"))
+        total_time = sum(time_values)
+        if total_time > 0:
+            job_id = result.split()[0]
+            result = subprocess.run(
+                        f"condor_history -l {job_id} | grep MachineAttrMachine0",
+                        shell=True,
+                        capture_output=True,
+                        check=True,
+                        text=True
+                    )
+            print(result.stdout.strip(), job_id)
 
 if __name__ == "__main__":
     main()
